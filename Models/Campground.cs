96b7a3dc728ad80ef494cc44f73e92cba0c20679
digitopoi <hubo85@gmail.com>
2017-10-03 20:06:19 -0500

    Add campground model

---
+++ Models/Campground.cs
@@ -0,0 +1,11 @@
+namespace YelpCamp_ASPNET_Angular.Models
+{
+    public class Campground
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
