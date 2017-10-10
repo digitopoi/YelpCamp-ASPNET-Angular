cd99c99017f7e8126f219fac192aaa2ac2dee551
digitopoi <hubo85@gmail.com>
2017-10-09 22:05:16 -0500

    Add seed data to program file, fix syntax errors in seed data

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using YelpCamp_ASPNET_Angular.Data;
 
 namespace YelpCamp_ASPNET_Angular.Models
@@ -41,7 +43,7 @@ namespace YelpCamp_ASPNET_Angular.Models
                             Id = 2,
                             Name = "Beach Bliss",
                             Price = "$35.00",
-                            Image = "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",,
+                            Image = "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         }
                     );
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using YelpCamp_ASPNET_Angular.Models;
 
 namespace YelpCamp_ASPNET_Angular
 {
@@ -14,7 +16,24 @@ namespace YelpCamp_ASPNET_Angular
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try 
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
